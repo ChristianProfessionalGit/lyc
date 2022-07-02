f8815a96682800f5c735af919a0a1ba3f7e8ab70
ChristianProfessionalGit <54475726+ChristianProfessionalGit@users.noreply.github.com>
2022-07-01 22:49:05 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
 flex Lexico.l
 bison -dyv sintactico.y
-gcc.exe lex.yy.c y.tab.c -o Primera.exe
-Primera.exe prueba.txt
+gcc.exe lex.yy.c y.tab.c -o Segunda.exe
+Segunda.exe prueba.txt
 
 @echo off
-del Primera.exe
+del Segunda.exe
 del lex.yy.c
 del y.tab.c
 del y.tab.h
